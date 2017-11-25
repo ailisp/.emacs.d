1dacb14c0b317fe93206ae40532b4f8350869f49
Bo Yao <icerove@gmail.com>
2017-11-25 10:11:37 -0500

    a barely useful c/c++/objc config

--- init/17-c.el
+++ init/17-c.el
@@ -9,27 +9,26 @@
                     #'(lambda ()
                         (rtags-install)))
 
-(require-or-install 'ac-rtags)
-(setq rtags-autostart-diagnostics t)
-(rtags-diagnostics)
-(setq rtags-completions-enabled t)
-(defun ac-rtags-prefix ()
-  (point))
-
 (require-or-install 'flycheck-rtags)
 (defun my-flycheck-rtags-setup ()
   (flycheck-select-checker 'rtags)
   (setq-local flycheck-highlighting-mode nil) ;; RTags creates more accurate overlays.
   (setq-local flycheck-check-syntax-automatically nil))
 
-(defun my-rtags-setup ()
-  (rtags-start-process-unless-running)
-  (push 'ac-source-rtags ac-source)
-  (my-flycheck-rtags-setup))
-
-(add-hook 'c-mode-hook #'my-rtags-setup)
-(add-hook 'c++-mode-hook #'my-rtags-setup)
-(add-hook 'objc-mode-hook #'my-rtags-setup)
-
 (require-or-install 'helm-rtags)
 (setq rtags-display-result-backend 'helm)
+
+(require-or-install 'auto-complete-clang)
+
+(defun my-rtags-setup ()
+  (rtags-start-process-unless-running)
+  (add-to-list 'ac-sources 'ac-source-clang)
+  (my-flycheck-rtags-setup))
+
+(define-key c-mode-base-map (kbd "M-.") 'rtags-find-symbol-at-point)
+(define-key c-mode-base-map (kbd "M-,") 'rtags-location-stack-back)
+(define-key c-mode-base-map (kbd "M-?") 'rtags-find-references)
+
+(add-hook 'c-mode-hook 'my-rtags-setup)
+(add-hook 'c++-mode-hook 'my-rtags-setup)
+(add-hook 'objc-mode-hook 'my-rtags-setup)
