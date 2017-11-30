39d2b95d3795a67d8b3b935909ad000699a46fc1
Bo Yao <icerove@gmail.com>
2017-11-30 00:06:30 -0500

    fix org mode key confliction with tabbar

--- init/60-org-mode.el
+++ init/60-org-mode.el
@@ -1,2 +1,8 @@
 ;;;; Org-mode specific settings
 (add-hook 'org-mode-hook (lambda () (setq truncate-lines nil)))
+(eval-after-load "org"
+  '(progn
+     (define-key org-mode-map (kbd "<M-left>") nil)
+     (define-key org-mode-map (kbd "<M-right>") nil)
+     (define-key org-mode-map (kbd "M-[") 'org-metaleft)
+     (define-key org-mode-map (kbd "M-]") 'org-metaright)))
