23f6c008b42ef9c94658784b39b00eda1730aa4f
Bo Yao <icerove@gmail.com>
2017-12-12 13:29:07 -0500

    ruby and rails

---
+++ init/23-ruby.el
@@ -0,0 +1,22 @@
+;;;; ruby
+
+(require-or-install 'inf-ruby)
+(require-or-install 'ruby-compilation)
+
+(define-key ruby-mode-map (kbd "C-c C-k") 'ruby-compilation-this-buffer)
+(define-key ruby-mode-map (kbd "C-c C-j") 'ruby-compilation-this-test)
+
+(defalias 'rake 'ruby-compilation-rake)
+
+(require-or-install 'robe)
+(add-hook 'ruby-mode-hook 'robe-mode)
+(add-hook 'robe-mode-hook 'ac-robe-setup)
+
+(require-or-install 'yari)
+(define-key ruby-mode-map (kbd "C-c C-d") 'yari-helm)
+
+(require-or-install 'goto-gem)
+(require-or-install 'bundler)
+
+(require-or-install 'projectile-rails)
+(projectile-rails-global-mode)
