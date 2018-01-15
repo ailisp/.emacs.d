d39b9cac0066eaeca7dbfb72f02fc56c1b5f8df8
Bo Yao <icerove@gmail.com>
2018-01-14 22:58:36 -0500

    auto pep8

--- init/22-python.el
+++ init/22-python.el
@@ -1,5 +1,6 @@
 (require-or-install 'anaconda-mode)
 (require-or-install 'company-anaconda)
+(require-or-install 'py-autopep8)
 
 (add-hook 'python-mode-hook
           (lambda ()
@@ -7,6 +8,7 @@
             (anaconda-eldoc-mode)
             (auto-complete-mode -1)
             (company-mode 1)
-            (add-to-list 'company-backends 'company-anaconda)))
+            (add-to-list 'company-backends 'company-anaconda)
+            (py-autopep8-enable-on-save)))
 
 (define-key anaconda-mode-map (kbd "M-,") 'anaconda-mode-go-back)
