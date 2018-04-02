e50cb7f991ba87060a89bba160dac9be3cf41803
Bo Yao <icerove@gmail.com>
2018-04-01 23:50:51 -0400

    bison

---
+++ init/18-bison.el
@@ -0,0 +1,13 @@
+(require-or-install 'bison-mode)
+
+(add-hook 'bison-mode-hook
+          (lambda ()
+            (setq imenu-create-index-function
+                  (lambda ()
+                    (let ((end))
+                      (beginning-of-buffer)
+                      (re-search-forward "^%%")
+                      (forward-line 1)
+                      (setq end (save-excursion (re-search-forward "^%%") (point)))
+                      (loop while (re-search-forward "^\\([a-z].*?\\)\\s-*\n?\\s-*:" end t)
+                            collect (cons (match-string 1) (point))))))))
