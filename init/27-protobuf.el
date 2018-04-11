5857093eef22c6ede0d665fe91ced6f32337eb7b
Bo Yao <byao@mymakecents.com>
2018-04-11 07:52:28 -0400

    protobuf

--- init/27-protobuf.el
+++ init/27-protobuf.el
@@ -1,1 +1,8 @@
 (require-or-install 'protobuf-mode)
+
+(defconst my-protobuf-style
+  '((c-basic-offset . 2)
+    (indent-tabs-mode . nil)))
+
+(add-hook 'protobuf-mode-hook
+          (lambda () (c-add-style "my-style" my-protobuf-style t)))
