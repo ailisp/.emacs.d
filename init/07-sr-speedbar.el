29c84a44e0b1f8c144fd6afab84320229dd3b96b
Bo Yao <byao@mymakecents.com>
2018-07-12 16:01:48 -0400

    basic working sr-speedbar

--- init/07-sr-speedbar.el
+++ init/07-sr-speedbar.el
@@ -2,11 +2,37 @@
 
 (setq sr-speedbar-right-side nil)
 (setq speedbar-show-unknown-files t)
-(setq sr-speedbar-width 35)
-(setq sr-speedbar-max-width 35)
+(setq sr-speedbar-width 25)
+(setq sr-speedbar-max-width 25)
 (setq speedbar-use-images nil)
 (setq speedbar-initial-expansion-list-name "files")
-(with-current-buffer sr-speedbar-buffer-name
-  (setq window-size-fixed 'width))
 
-(global-set-key (kbd "C-x 3") 'sr-speedbar-toggle)
+(global-set-key (kbd "C-x 3")
+                '(lambda ()
+                   (interactive)
+                   (sr-speedbar-toggle)
+                   (when (sr-speedbar-exist-p)
+                     (with-current-buffer sr-speedbar-buffer-name
+                       (setq window-size-fixed 'width)))))
+
+;; (defun resize-speedbar-on-window-size-changed (&optional frame)
+;;   (when (sr-speedbar-exist-p)
+;;     (when (or (/= (window-pixel-width-before-size-change)
+;;                   (window-pixel-width))
+;;               (/= (window-pixel-height-before-size-change)
+;;                   (window-pixel-height)))
+;;       ;; (sr-speedbar-close)
+;;       (with-current-buffer sr-speedbar-buffer-name
+;;         (setq window-size-fixed nil))
+;;       (setq sr-speedbar-width
+;;             (max 30 (min 35 (round (* 0.3 (window-width))))))
+;;       (setq sr-speedbar-max-width
+;;             sr-speedbar-width)
+;;       (with-current-buffer sr-speedbar-buffer-name
+;;         (setq window-size-fixed 'width))
+;;       (setq ring-bell-function nil)
+;;       (ding)
+;;       ;; (sr-speedbar-open)
+;;       )))
+
+;; (add-to-list 'window-size-change-functions #'resize-speedbar-on-window-size-changed)
