59e9d67a30a5c6b7a9c41722f74b7c5b4f42e1b5
Bo Yao <icerove@gmail.com>
2018-07-21 09:53:37 +0800

    init theme for dasung

--- init/05-theme.el
+++ init/05-theme.el
@@ -1,30 +1,31 @@
-;;;; Theme
+;; ;;;; Theme
 
-(load-theme 'misterioso)
-(when (windowsp)
-  (set-face-attribute
-   'default nil
-   :family "DejaVu Sans Mono"))
-(when (macp)
-  (set-face-attribute
-   'default nil
-   :height 140
-   :family "DejaVu Sans Mono"))
+(load-theme 'whiteboard)
+;; (load-theme 'misterioso)
+;; (when (windowsp)
+;;   (set-face-attribute
+;;    'default nil
+;;    :family "DejaVu Sans Mono"))
+;; (when (macp)
+;;   (set-face-attribute
+;;    'default nil
+;;    :height 140
+;;    :family "DejaVu Sans Mono"))
 
-;;; For some reason, cursor face doesn't change in new window
-;;; call it after create new frame
-(defun set-cursor-face ()
-  (set-cursor-color "sky blue"))
-(set-cursor-face)
-(add-hook 'after-make-frame-functions
-          (lambda (f)
-            (with-selected-frame f
-              (set-cursor-face))))
+;; ;;; For some reason, cursor face doesn't change in new window
+;; ;;; call it after create new frame
+;; (defun set-cursor-face ()
+;;   (set-cursor-color "sky blue"))
+;; (set-cursor-face)
+;; (add-hook 'after-make-frame-functions
+;;           (lambda (f)
+;;             (with-selected-frame f
+;;               (set-cursor-face))))
 
-(set-face-attribute
- 'mode-line-inactive nil
- :background "#435364")
-(set-face-attribute
- 'header-line nil
- :background "#506478"
- :foreground "white")
+;; (set-face-attribute
+;;  'mode-line-inactive nil
+;;  :background "#435364")
+;; (set-face-attribute
+;;  'header-line nil
+;;  :background "#506478"
+;;  :foreground "white")
--- init/07-helm.el
+++ init/07-helm.el
@@ -30,10 +30,10 @@
 (global-set-key [remap execute-extended-command] #'helm-smex)
 (global-set-key (kbd "M-X") #'helm-smex-major-mode-commands)
 
-(custom-set-faces
- '(helm-ff-directory ((t (:foreground "white"))))
- '(helm-ff-dotted-directory ((t (:foreground "white"))))
- '(helm-ff-dotted-symlink-directory ((t (:background "DimGray" :foreground "font-lock-function-name-face"))))
- '(helm-selection ((t (:background "sky blue" :foreground "#212931" :weight bold))))
- '(helm-source-header ((t (:inherit mode-line-inactive :foreground "white" :weight bold :height 1.3 :family "Sans Serif"))))
- '(helm-candidate-number ((t (:foreground "yellow")))))
+;; (custom-set-faces
+;;  '(helm-ff-directory ((t (:foreground "white"))))
+;;  '(helm-ff-dotted-directory ((t (:foreground "white"))))
+;;  '(helm-ff-dotted-symlink-directory ((t (:background "DimGray" :foreground "font-lock-function-name-face"))))
+;;  '(helm-selection ((t (:background "sky blue" :foreground "#212931" :weight bold))))
+;;  '(helm-source-header ((t (:inherit mode-line-inactive :foreground "white" :weight bold :height 1.3 :family "Sans Serif"))))
+;;  '(helm-candidate-number ((t (:foreground "yellow")))))
