45c0ca386782f6edccc625001aa3414d8f170d7d
Bo Yao <icerove@gmail.com>
2018-08-23 16:49:49 -0400

    remove ace jump and move projectile into ivy

--- init/03-avy.el
+++ init/03-avy.el
@@ -4,6 +4,7 @@
 (global-set-key (kbd "C-'") 'avy-goto-word-1)
 (global-set-key (kbd "M-g g") 'avy-goto-line)
 
+
 (require-or-install 'switch-window)
 (setq-default switch-window-shortcut-style 'alphabet)
 (setq-default switch-window-timeout nil)
--- init/07-ivy.el
+++ init/07-ivy.el
@@ -1,13 +1,26 @@
 ;; counsel depends on swiper and swiper depends on ivy
+(require-or-install 'ivy)
+(require-or-install 'swiper)
 (require-or-install 'counsel)
 (require-or-install 'ivy-hydra)
 
 (ivy-mode)
 (setq ivy-use-virtual-buffers t)
 (setq ivy-re-builders-alist
-      '((t . ivy--regex-fuzzy)))
+      '((read-file-name-internal . ivy--regex-fuzzy)
+        (execute-extended-command . ivy--regex-fuzzy)
+        (ivy-switch-buffer . ivy--regex-fuzzy)
+        (t . ivy--regex-plus)))
 
 (global-set-key (kbd "C-s") 'swiper)
 (global-set-key (kbd "C-r") 'swiper-all)
 (global-set-key (kbd "C-x C-b") 'counsel-locate)
 (global-set-key (kbd "C-x g") 'counsel-rg)
+
+(require-or-install 'ivy-xref)
+(setq xref-show-xrefs-function 'ivy-xref-show-xrefs)
+
+(require-or-install 'counsel-projectile)
+(counsel-projectile-mode)
+(define-key projectile-mode-map (kbd "C-x p") 'projectile-command-map)
+(define-key projectile-command-map (kbd "g") 'counsel-projectile-rg)
