6b7bf98275f6f33d4c667911bffbb4767b379b42
Bo Yao <byao@mymakecents.com>
2018-08-22 23:18:57 -0400

    rg

--- init/15-projectile.el
+++ init/15-projectile.el
@@ -4,3 +4,8 @@
 (projectile-mode)
 (define-key projectile-mode-map (kbd "C-x p") 'projectile-command-map)
 (helm-projectile-on)
+
+(require-or-install 'ripgrep)
+(require-or-install 'projectile-ripgrep)
+(define-key projectile-command-map (kbd "g") 'projectile-ripgrep)
+(global-set-key (kbd "C-x g") 'ripgrep-regexp)
