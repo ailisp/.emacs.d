72953d6bafbb30f0e3f37d662408584ac1211c51
Bo Yao <icerove@gmail.com>
2018-08-25 11:09:26 -0400

    a few update

--- init/03-telephone-line.el
+++ init/03-telephone-line.el
@@ -1,10 +1,31 @@
 (require-or-install 'telephone-line)
 (setq telephone-line-lhs
-  '((accent . (telephone-line-vc-segment
-               telephone-line-erc-modified-channels-segment
-               telephone-line-process-segment))
-    (nil    . (telephone-line-projectile-segment
-               telephone-line-buffer-segment))))
+      '((accent . (telephone-line-buffer-segment))
+        (nil    . (telephone-line-major-mode-segment))))
+
+(setq telephone-line-rhs
+      '((nil    . (telephone-line-flycheck-segment
+                   telephone-line-projectile-segment))
+        (accent . (telephone-line-vc-segment))))
+
+(telephone-line-defsegment telephone-line-projectile-segment ()
+  "Displays the current project name, according to projectile."
+  (if (fboundp 'projectile-project-name)
+      (propertize (projectile-project-name)
+                  'face 'telephone-line-projectile
+                  'display '(raise 0.0)
+                  'help-echo "Switch project"
+                  'mouse-face '(:box 1)
+                  'local-map (make-mode-line-mouse-map
+                              'mouse-1 (lambda ()
+                                         (interactive)
+                                         (counsel-projectile-switch-project))))))
+(set-face-attribute
+ 'telephone-line-accent-active nil
+ :background "#43655d")
+(set-face-attribute
+ 'telephone-line-projectile nil
+ :inherit nil)
 (set-face-attribute
  'telephone-line-accent-inactive nil
  :background "#435364")
