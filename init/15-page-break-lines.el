b8b983de377d7cfc11aa18648afd4db914362757
Bo Yao <byao@mymakecents.com>
2018-08-28 11:55:27 -0400

    clj refactor and page break lines

---
+++ init/15-page-break-lines.el
@@ -0,0 +1,4 @@
+;;;; page-break-lines
+
+(require-or-install 'page-break-lines)
+(global-page-break-lines-mode 1)
--- init/20-clojure.el
+++ init/20-clojure.el
@@ -5,19 +5,28 @@
 (require-or-install 'elein)
 
 (add-hook 'clojure-mode-hook 'subword-mode)
+(require 'smartparens)
+(add-hook 'clojure-mode-hook 'smartparens-strict-mode)
 
 (require-or-install 'cider)
 (setq cider-prompt-for-symbol nil)
+;; cider-mode is a minor mode when a clj/cljs buffer has linked REPL
 (add-hook 'cider-mode-hook 'eldoc-mode)
 (add-hook 'cider-repl-mode-hook 'subword-mode)
 (add-hook 'cider-repl-mode-hook 'smartparens-strict-mode)
-(add-hook 'clojure-mode-hook 'smartparens-strict-mode)
 
 (require 'company)
 (add-hook 'cider-repl-mode-hook 'company-mode)
 (add-hook 'cider-mode-hook 'company-mode)
 (add-hook 'cider-repl-mode-hook 'cider-company-enable-fuzzy-completion)
 (add-hook 'cider-mode-hook 'cider-company-enable-fuzzy-completion)
+
+(require-or-install 'clj-refactor)
+(defun enable-clj-refactor ()
+  (clj-refactor-mode 1)
+  (yas-minor-mode 1)
+  (cljr-add-keybindings-with-prefix "C-c r"))
+(add-hook 'clojure-mode-hook 'enable-clj-refactor)
 
 ;; TODO: need some config to make flycheck working
 ;; (require-or-install 'flycheck-clojure)
