832d91d24f0a3c847d66fad7e1f6fb09d2351eee
Bo Yao <byao@mymakecents.com>
2018-09-18 11:18:58 -0400

    more rust

--- init/18-rust.el
+++ init/18-rust.el
@@ -1,9 +1,17 @@
 (require-or-install 'rust-mode)
 (setq rust-format-on-save t)
 
+(require-or-install 'cargo)
+(require-or-install 'racer)
+
+(add-hook 'rust-mode-hook #'racer-mode)
+(add-hook 'racer-mode-hook #'eldoc-mode)
+
+(require-or-install 'company-mode)
+(add-hook 'racer-mode-hook #'company-mode)
+
 (require-or-install 'flycheck-rust)
 (with-eval-after-load 'rust-mode
   (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
 
-(require-or-install 'cargo)
-(require-or-install 'racer)
+(require-or-install 'rust-snippets)
