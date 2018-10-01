027bf51da331ab8f8197c576cefed2f462e106b0
Bo Yao <byao@mymakecents.com>
2018-10-01 18:04:51 -0400

    some solidity update

--- init/30-solidity.el
+++ init/30-solidity.el
@@ -1,2 +1,6 @@
 (require-or-install 'solidity-mode)
 (require-or-install 'company-solidity)
+
+(define-key solidity-mode-map (kbd "C-c g") 'solidity-estimate-gas-at-point)
+(add-hook 'solidity-mode #'flycheck-mode)
+(setq solidity-flycheck-solc-checker-active t)
