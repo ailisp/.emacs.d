ccf58448c69b6ad527e3517f2e645a476c2f445b
Bo Yao <icerove@gmail.com>
2019-03-09 11:52:46 -0500

    mostly ok paperlike hd theme

--- init/01-theme.el
+++ init/01-theme.el
@@ -1,3 +1,12 @@
 ;;;; Theme
 
-(load-theme 'bo-dark)
+(defvar current-theme 'bo-dark)
+(load-theme current-theme t)
+
+(defun toggle-light-dark ()
+  (interactive)
+  (setf current-theme
+        (if (eql current-theme 'bo-dark)
+            'bo-light
+          'bo-dark))
+  (load-theme current-theme t))
