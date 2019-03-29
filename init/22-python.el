b9c79cb0cbfd7d590f1443cb1df4978af06d27b9
Bo Yao <byao@mymakecents.com>
2019-03-29 10:34:15 -0400

    by language server c, c++ and python usable in emacs

--- init/22-python.el
+++ init/22-python.el
@@ -1,12 +1,2 @@
-(require-or-install 'anaconda-mode)
-(require-or-install 'company-anaconda)
-(require-or-install 'py-autopep8)
-
-(add-hook 'python-mode-hook
-          (lambda ()
-            (anaconda-mode)
-            (anaconda-eldoc-mode)
-            (add-to-list 'company-backends 'company-anaconda)
-            (py-autopep8-enable-on-save)))
-
-(define-key anaconda-mode-map (kbd "M-,") 'anaconda-mode-go-back)
+;; pip install 'python-language-server[all]'
+(add-hook 'python-mode-hook #'lsp)
